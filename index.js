56ff52ef0693b6757c5fb3856137e9bcec6d2825
Harley Turan <harley@hturan.com>
2015-04-04 02:00:47 +0100

    Refactored http and ws server init

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 3000});
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var finalhandler = require("finalhandler")
+var serveStatic = require("serve-static")
+
+var port = process.env.PORT || 5000
+
+var serve = serveStatic('./', {'index': ['index.html']})
+ 
+// Create server 
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+})
+ 
+// Listen 
+server.listen(port);
+
+var wss = new WebSocketServer({server: server});
 
 var clients = [];
 
@@ -17,23 +34,6 @@ wss.on('connection', function(ws) {
     sendMessage('upDown.motion', {value: rotation.x});
   });
 });
-
-
-var finalhandler = require('finalhandler')
-var http = require('http')
-var serveStatic = require('serve-static')
- 
-// Serve up public/ftp folder 
-var serve = serveStatic('./', {'index': ['index.html']})
- 
-// Create server 
-var server = http.createServer(function(req, res){
-  var done = finalhandler(req, res)
-  serve(req, res, done)
-})
- 
-// Listen 
-server.listen(process.env.PORT || 8000);
 
 // var dualShock = require('dualshock-controller');
 
