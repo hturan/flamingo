d7e65eb9a7ff2b1350b6c745af1497a46fc2e5f8
Harley Turan <harley@hturan.com>
2015-04-04 01:49:48 +0100

    Added static server to node app

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ wss.on('connection', function(ws) {
   });
 });
 
+
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+ 
+// Serve up public/ftp folder 
+var serve = serveStatic('./', {'index': ['index.html']})
+ 
+// Create server 
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+})
+ 
+// Listen 
+server.listen(process.env.PORT | 8000);
+
 // var dualShock = require('dualshock-controller');
 
 // var controller = dualShock({
